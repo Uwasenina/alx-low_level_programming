386401874ace622f8c42f5f39acc42009742e064
uwasenina <uwasenina63@gmail.com>
2022-03-10 14:11:17 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - printS phase
+ *
+ * Return: 0 if exited proparly, otherwise non-zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
