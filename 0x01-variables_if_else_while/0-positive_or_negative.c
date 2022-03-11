86e9dea8624d73c13340da782757ef3c63817a64
uwasenina <uwasenina63@gmail.com>
2022-03-11 11:02:05 -0800

    Check whether the number is negative,positive or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - To check if a number is negative or positive
+ *
+ * Return: 0 if it exited, otherwise non-zero
+ */
+int main(void)
+{
+	int n;
+
+	printf("Enter the number to be checked\n");
+	scanf("%d", n);
+	if (n > 0) /* checking whether it is positive*/
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0) /*check whether it is zero*/
+	{
+		printf("%d is zero\n", n);
+	}
+	else /* otherwise it is negstive*/
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
