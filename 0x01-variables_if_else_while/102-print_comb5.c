d3bc01ecbd7cc0bb4d66b5f5130f81b401dc2c10
uwasenina <uwasenina63@gmail.com>
2022-03-12 07:12:16 -0800

    Combination of two two-digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,16 +13,19 @@ int main(void)
 	{
 		for (j = i + 1; j < 100; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			if (i == 98 && i == 99)
-				break;
-
-			putchar(',');
-			putchar(' ');
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
