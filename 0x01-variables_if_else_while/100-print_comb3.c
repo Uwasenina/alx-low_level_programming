3233b544c35c723a2f5f72b1ebc158adc6eb0e4d
uwasenina <uwasenina63@gmail.com>
2022-03-12 04:22:50 -0800

    Combination of two difference digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 48; digit1 < 57; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 58; digit2++)
 		{
 			putchar(digit1);
 			putchar(digit2);
 
-			if (digit1 == 8 && digit2 == 9)
+			if (digit1 == 56 && digit2 == 57)
 				break;
 
 			putchar(',');
