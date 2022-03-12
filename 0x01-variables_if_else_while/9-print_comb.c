e0a54a8e31ca24592843f657f65a7dc5c156eb88
uwasenina <uwasenina63@gmail.com>
2022-03-12 03:09:57 -0800

    Combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,11 @@ int main(void)
 	for (n = 0; n < 10; n++)
 	{
 		putchar(n);
-		if (n == 9)
-			continue;
-
+		if (n != 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
