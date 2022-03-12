b21f155b0509aa67e3a0b3f19e41be0ebdc268c0
uwasenina <uwasenina63@gmail.com>
2022-03-12 01:30:12 -0800

    Check whether it is zero, positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - assigns a random number to int n everytime
@@ -9,8 +11,9 @@ int main(void)
 {
 	int n;
 
-	printf("Enter a number to be checked\n");
-	scanf("%d", &n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
