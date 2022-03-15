925eb4be85b04622f11c13a8902b4a81beb12987
uwasenina <uwasenina63@gmail.com>
2022-03-15 12:12:47 -0700

    Pint _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Return: 0 0n success
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
