860d71c8f6f568d8dca027f9e99a4923f2b45d22
uwasenina <uwasenina63@gmail.com>
2022-03-15 15:37:35 -0700

    Uppercase and Lowercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 
 /**
  * _isalpha - tests whether a character is from english alpabet
- * @c: ASCII character to be checked.
+ * @c: character to be checked.
  * Return: 1 if a character is an english alphabet, else 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c >= 90) || (c <= 97 && c >= 122))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
