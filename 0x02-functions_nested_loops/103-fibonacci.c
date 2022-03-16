a2932a67ed6e3ebf6e602e7f16743e5e0ff17eb1
uwasenina <uwasenina63@gmail.com>
2022-03-16 06:33:25 -0700

    Sum of even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of the even-valued terms not exceeding 4000000
+ * Return: 0 on success
+ */
+int main(void)
+{
+	unsigned long i = 0, j = 1, k;
+	long tot_sum = 0;
+
+	while (1)
+	{
+		k = i + j;
+		if (k > 4000000)
+		{
+			break;
+		}
+		if ((k % 2) == 0)
+		{
+			tot_sum = tot_sum + k;
+		}
+		i = j;
+		j = k;
+	}
+	printf("%lu", tot_sum);
+	return (0);
+}
