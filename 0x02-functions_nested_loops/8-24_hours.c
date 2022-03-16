4888a6ae9b9241c958f42c4d89baf942ba290b78
uwasenina <uwasenina63@gmail.com>
2022-03-15 18:58:10 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 48; a <= 50; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 48; b <= 57; b++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+			if ((a <= 49 && b <= 57) || (a <= 50 && b <= 51))
 			{
-				for (c = 0; c <= 5; c++)
+				for (c = 48; c <= 53; c++)
 				{
-					for (d = 0; d <= 9; d++)
+					for (d = 48; d <= 57; d++)
 					{
 						_putchar(a + '0');
 						_putchar(b + '0');
@@ -27,5 +27,5 @@ void jack_bauer(void)
 				}
 			}
 		}
-		\}
 	}
+}
