d9bf2de85b89391a51b7e17576a7b7ecaea1080f
uwasenina <uwasenina63@gmail.com>
2022-03-15 23:01:52 -0700

    9 Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table starting from 0
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int i, j, k, a, b;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+			a = k / 10;
+			b = k % 10;
+			if (k != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (k >= 10)
+			{
+				_putchar(a + '0');
+				_putchar(b + '0');
+			}
+			else if (k < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(b + '0');
+			}
+			else
+			{
+				_putchar(b + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
