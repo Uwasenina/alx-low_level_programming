e0d606427def78c1f41e84ad84802fb5c37bee55
uwasenina <uwasenina63@gmail.com>
2022-03-16 06:49:56 -0700

    Sum of fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,19 +9,16 @@ int main(void)
 	unsigned long i = 0, j = 1, k;
 	long tot_sum = 0;
 
-	while (1)
+	k = 0;
+	while (k <= 4000000)
 	{
 		k = i + j;
-		if (k > 4000000)
-		{
-			break;
-		}
+		i = j;
+		j = k;
 		if ((k % 2) == 0)
 		{
 			tot_sum = tot_sum + k;
 		}
-		i = j;
-		j = k;
 	}
 	printf("%ld", tot_sum);
 	return (0);
