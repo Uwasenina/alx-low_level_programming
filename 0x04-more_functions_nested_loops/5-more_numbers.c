4e202a640f4f54c5e5c14bf6420d9fe2938060ac
uwasenina <uwasenina63@gmail.com>
2022-03-17 14:39:28 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
@@ -16,7 +16,7 @@ void more_numbers(void)
 			{
 				_putchar('1');
 			}
-			_putchar((j % 10 + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
