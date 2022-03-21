97894f69136d1f77ad10b29ac791ae157d3be82c
uwasenina <uwasenina63@gmail.com>
2022-03-21 15:37:21 -0700

    My personal work

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - print reverse character
+ * @str: character
+ * Return: void
+ */
+voit puts_half(char *str)
+{
+	int i = 0, n;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		n = (i + 1) / 2;
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		n = i / 2;
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
