8616e1ea25670ab31ec247eb7155ecf99e537c36
uwasenina <uwasenina63@gmail.com>
2022-03-24 05:17:56 -0700

    Print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,5 +9,13 @@ void print_number(int n)
 {
 	unsigned int x = n;
 
-	_putchar(x + '\0');
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	if ((x / 10) > 0)
+	{
+		print_number(x / 10);
+	}
+	_putchar(x % 10 + '0');
 }
