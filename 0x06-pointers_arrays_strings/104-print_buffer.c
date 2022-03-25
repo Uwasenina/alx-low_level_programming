4de565b46cd629f9456e9f1a23213b0960b6939b
uwasenina <uwasenina63@gmail.com>
2022-03-25 08:38:36 -0700

    Buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -18,38 +18,26 @@ void print_buffer(char *b, int size)
 		for (i = 0; i < 10; i++)
 		{
 			if ((i + x) >= size)
-			{
 				printf(" ");
-			}
 			else
-			{
 				printf("%02x", *(b + i + x));
-			}
 			if ((i % 2) != 0 && i != 0)
-			{
 				printf(" ");
-			}
 		}
 		for (i = 0; i < 10; i++)
 		{
 			if ((i + x) >= size)
-			{
 				break;
-			}
-			else if ((*(b + i + x) >= 31 && *(b + i + x) <= 126)
-					{
-					printf("%c", *(b + i + x));
-					}
+			else if (*(b + i + x) >= 31 && *(b + i + x) <= 126)
+					printf("%c", *(b + i + x));	
 					else
-					{
 					printf(" ");
-					}
-					}
+		}
 					if (x >= size)
 					continue;
 
 					printf("\n");
-					}
+	}
 					if (size <= 0)
 					printf("\n");
-
+}
