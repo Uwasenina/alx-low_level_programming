37d905dead3142904c654730b1a0b3b3aba394ee
uwasenina <uwasenina63@gmail.com>
2022-04-06 08:11:22 -0700

    STATIC LIBRARIES

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value
+ * @x: integer
+ * Return: value
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * -1);
+	}
+}
