4b7bde8517629d88a1f8a6e11c3e76030287e813
uwasenina <uwasenina63@gmail.com>
2022-04-07 19:36:58 -0700

    MULTIPLY

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -56,3 +56,22 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 		return (NULL);
 	return (dest);
 }
+/**
+ * check_for_digits - checks the agruments
+ * @av: pointer to arguments
+ * Return: 0 if digits, 1 if not
+ */
+int check_for_digits(char **av)
+{
+	int i, j;
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			if (av[i][j] < '0')
+				return (1);
+		}
+	}
+	return (0);
+}
