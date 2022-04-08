a2696deafa267ab889a6eedb61f004cc3dd4f0b5
uwasenina <uwasenina63@gmail.com>
2022-04-07 20:04:32 -0700

    Multiply two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -89,4 +89,49 @@ void init(char *str, int l)
 		str[i] = '0';
 	str[i] = '\0';
 }
+/**
+ * main - multiply two numbers
+ * @argc: number of arguments
+ * @argv: name of array
+ * Return: 0, or exit 98 if failure
+ */
+int main(int argc, char *argv[])
+{
+	int l1, l2, ln, ti, i;
+	char *p;
+	char *t;
+	char e[] = "Error\n";
 
+	if (argc != 3 || check_for_digits(argv))
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	ln = l1 + l2 + 1;
+	p = malloc(ln * sizeof(char));
+	if (p == NULL)
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	init(p, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	{
+		t = malloc(argv[2][ti], argv[1], l1 - 1, p, (l2 - 2) - i);
+		if (t == NULL)
+		{
+			for (ti = 0; e[ti]; ti++)
+				_putchar(e[ti]);
+			free(p);
+			exit(98);
+		}
+	}
+	_print(p, ln - 1);
+	return (0);
+}
