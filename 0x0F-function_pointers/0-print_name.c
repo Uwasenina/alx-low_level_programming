8152b3c284bcfb70ebf304dae87ec3e8617a7ddf
uwasenina <uwasenina63@gmail.com>
2022-04-14 05:33:05 -0700

    My Name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: char pointer
- * @f: function to  print with
+ * print_name - prints name
+ * @name: char
+ * @f: function to print
  * Return: void
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
