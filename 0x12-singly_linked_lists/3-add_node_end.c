a7a77399cae0c3ab63fd8714560ffb52880d50e4
uwasenina <uwasenina63@gmail.com>
2022-04-21 14:45:04 -0700

    Adds new node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,17 +1,19 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
- * *add_node -functions that adds new node at the beginning of a list
- * @str: string for add in the node
+ * *add_node_end - function that adds a new at the end of a list
  * @head: node to add
+ * @str: string
  * Return: the address of the new element
  */
-list_t *add_node(list_t **head, const char *str)
+
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
+	list_t *end = NULL;
 	char *string;
 
 	new = malloc(sizeof(list_t));
@@ -25,24 +27,30 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new->str = string;
 	new->len = _strlen(string);
-	new->next = *head;
-	*head = new;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	end = *head;
+	while (end->next != NULL)
+		end = end->next;
+	end->next = new;
 
 	return (new);
 }
-
 /**
- * _strlen - function that indicates the length of the char
+ * _strlen - function that indicates the lengthof the char
  * @str: char
- * Return: length of char
+ * Return: the length of the char
  */
 
 int _strlen(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i] = '\0')
 		i++;
-
 	return (i);
 }
