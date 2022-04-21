5ab48ce59526fcada090cc2635e3558a82b35a33
uwasenina <uwasenina63@gmail.com>
2022-04-21 15:13:07 -0700

    Print a sentences

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * start - function that print before main function
+ * Return: void
+ */
+void start(void) __attribute__ ((constructor));
+
+void start(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
