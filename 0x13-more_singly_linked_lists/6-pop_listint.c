ecc0c83ff81838235649efe8658394a28cf2486d
uwasenina <uwasenina63@gmail.com>
2022-04-25 16:32:01 -0700

    Delete the head of node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete a head of node of linked list
+ * @head: pointer the head of node
+ * Return: the head of node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *node_to_delete;
+	int i;
+
+	if (*head == NULL)
+		return (0);
+
+	node_to_delete = *head;
+	*head = node_to_delete->next;
+	i = node_to_delete->i;
+	free(node_to_delete);
+
+	return (i);
+}
