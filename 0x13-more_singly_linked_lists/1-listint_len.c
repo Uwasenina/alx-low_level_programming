3fe0e1e3b9dad5eb9a6a8fbfec1fd4a6dd23145c
uwasenina <uwasenina63@gmail.com>
2022-04-25 07:02:19 -0700

    Linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in linked list
+ * @h: address of the first item in the list
+ * Return: the length of the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len;
+
+
+	for (len = 0; h = h->next)
+		len++;
+
+	return (len);
+}
