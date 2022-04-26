c9eeabb98cde8a5e032bf33e85807484bf9a46cd
uwasenina <uwasenina63@gmail.com>
2022-04-25 17:03:38 -0700

    Get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index: returns the nth node of a linked list
+ * get_nodeint_at_index - returns the nth node of a linked list
  * @head: pointer to first node of the linked list
  * @index: index of node to returned
  * Return: the nth node of a linked list
