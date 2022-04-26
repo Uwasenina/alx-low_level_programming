71ca6407e78b5091b8037af002b4d90fe8020eb4
uwasenina <uwasenina63@gmail.com>
2022-04-25 23:53:47 -0700

    Insert a new element

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert a new node at a given position
+ * @head: pointer to the list
+ * @idx: the index of the list
+ * @n: element/property in the list
+ * Return: Address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *current_node = *head, *new_node_ptr;
+
+	if (idx == 0)
+	{
+		new_node_ptr = malloc(sizeof(listint_t));
+
+		if (!new_node_ptr)
+			return (NULL);
+		new_node_ptr->n = n;
+		new_node_ptr->next = *head;
+		*head = new_node_ptr;
+		return (new_node_ptr);
+	}
+
+	while (current_node && ((i + 1) != idx))
+	{
+		current_node = current_node->next;
+		i++;
+	}
+	if ((i + 1) == idx)
+	{
+		new_node_ptr = malloc(sizeof(listint_t));
+
+		if (!new_node_ptr)
+			return (NULL);
+		new_node_ptr->n = n;
+		new_node_ptr->next = current_node->next;
+		current_node->next = new_node_ptr;
+		return (new_node_ptr);
+	}
+	return (NULL);
+}
