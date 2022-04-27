c52fba6ddbe21b4bde5e8f1d6e4e8cbfc2e02a86
uwasenina <uwasenina63@gmail.com>
2022-04-26 20:54:50 -0700

    Find loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,6 +7,23 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	head++;
-	return (0);
+	listint_t *fast = head;
+	listint_t *slow = head;
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (fast != slow)
+			{
+				fast = fast->next;
+				slow = slow->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
 }
