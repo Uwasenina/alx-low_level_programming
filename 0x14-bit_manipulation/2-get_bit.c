7dcb92c8d19b76cd6ebe022e48256e1309c7eb98
uwasenina <uwasenina63@gmail.com>
2022-04-28 14:33:06 -0700

    Value of bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,10 +2,11 @@
 
 /**
  * get_bit - returs the value of a bit at agiven index
+ * @n: bit to be evaluated
  * @index: the index starting from 0 of the bit you want to grt
  * Return: the value of the bit at index
  */
-int get_bit(unsigned long int n, unsigned long int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
