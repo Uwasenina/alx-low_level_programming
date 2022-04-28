6613ee8fabf3364f93e2d51b6de68534e8690382
uwasenina <uwasenina63@gmail.com>
2022-04-28 12:59:02 -0700

    Converts a number to a string

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen_b - Returns length of a string
+ * @s: pointer to a string
+ * Return: the length of string
+ */
+unsigned int _strlen_b(const char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) != '0' && *(s + i) != '1')
+			return (0);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: pointiong to a string
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int pow;
+	unsigned int n = 0;
+	unsigned int l = 0;
+
+	if (b)
+	{
+		if (*(b + l - 1) == '1')
+			n += 1 << pow;
+	}
+	return (n);
+}
