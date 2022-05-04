ddeca41d3309b7f490061cfed5e3842f4ecb4f7a
uwasenina <uwasenina63@gmail.com>
2022-05-04 15:50:34 -0700

    ELF

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,36 @@
 #include "main.h"
+#define REV(n) ((n << 24) | (((n >> 16) << 24) >> 16) | \ (((n << 16) >> 24) << 16) | (n >> 24))
+/**
+ * verify - verify the file to check if is a ELF
+ * @e_ident: the ELF struct
+ * return: void
+ */
+void verify(unsigned char *e_ident)
+{
+	if (*(e_ident) == 0x7f && *(e_ident + 1) == 'E' && *(e_ident + 2) == 'L' && *(e_ident + 3) == 'F')
+		printf("ELF Header:\n");
+	else
+	{
+		dprintf(STDERR_FILENO, "Error: this is not s valid ELF\n");
+		exit(98);
+	}
+}
+/**
+ * magic - print a magic number
+ * @e_ident: the ELF struct
+ * Return: void
+ */
+void magic(unsigned char *e_ident)
+{
+	int i;
+	int limit;
+
+	limit = EI_NIDENT - 1;
+	printf("  Magic:  ");
+	for (i = 0; i < limit, i++)
+		printf("%02x ", *(e_ident + i));
+	printf("%02x\n", *(e_ident + i));
+}
 
 /**
  * main - copies the content of a fioe
