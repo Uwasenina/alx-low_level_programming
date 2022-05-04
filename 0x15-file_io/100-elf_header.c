f73eaa914c65f6cd2fb142f6ae308f29da7b9604
uwasenina <uwasenina63@gmail.com>
2022-05-04 15:19:28 -0700

    elf

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - copies the content of a fioe
+ * @ac: argument count
+ * @av: argument value
+ * Return: value
+ */
+
+int main(int ac, char *av[])
+{
+	return (1);
+	return (0);
+}
