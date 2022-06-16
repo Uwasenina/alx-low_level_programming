2a1a4781ffa96b3570d708ad545d319c422c9de3
uwasenina <uwasenina63@gmail.com>
2022-06-15 22:22:02 -0700

    Calling function

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - compute the sum of two numbers
+ * @left: left-sided number
+ * @right: right-sidede number
+ * Return: results
+ */
+int add(int left, int right)
+{
+	return (left + right);
+}
+
+/**
+ * sub - compute the difference of two numbers
+ * @left: lift-sided number
+ * @right: right-sided number
+ * Return: result
+ */
+int sub(int left, int right)
+{
+	return (left - right);
+}
+
+/**
+ * mul - compute the product of two numbers
+ * @left: integer
+ * @right: integer
+ * Return: result
+ */
+int mul(int left, int right)
+{
+	return (left * right);
+}
+
+/**
+ * div - compute the division of two numbers
+ * @left: integer
+ * @right: integer
+ * Return: result
+ */
+int div(int left, int right)
+{
+	return (left / right);
+}
+
+/**
+ * mod - compute the modulus of two numbers
+ * @left: integer
+ * @right: integer
+ * Return: results
+ */
+int mod(int left, int right)
+{
+	return (left % right);
+}
